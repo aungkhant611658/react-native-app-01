048863427a03463ad7d9e2658cab62e254648441
aungkhant611658 <aungkhant611658@gmail.com>
2024-08-26 18:56:06 +0630

    setup route

--- app/index.jsx
+++ app/index.jsx
@@ -4,7 +4,7 @@ import { StatusBar, Text, View } from "react-native";
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl">Hello World!</Text>
+      <Text className="text-3xl font-pblack">Hello World!</Text>
       <StatusBar style="auto" />
       <Link href="/profile" style={{ color: "blue" }}>
         Go to Profile
