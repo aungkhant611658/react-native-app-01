b5e4fdefc720f6fdf083ca19b57bda96cde4b709
aungkhant611658 <aungkhant611658@gmail.com>
2024-08-26 14:11:48 +0630

    setup route

--- app/index.jsx
+++ app/index.jsx
@@ -1,15 +1,23 @@
-import { Text, View } from "react-native";
+import { Link } from "expo-router";
+import { StatusBar, StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <View style={styles.container}>
       <Text>Hello World!</Text>
+      <StatusBar style="auto" />
+      <Link href="/profile" style={{ color: "blue" }}>
+        Go to Profile
+      </Link>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
