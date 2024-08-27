3193860c24e59a825785be1fe6517884d420faf4
aungkhant611658 <aungkhant611658@gmail.com>
2024-08-27 13:00:12 +0630

    home page screen design

--- app/index.jsx
+++ app/index.jsx
@@ -1,26 +1,30 @@
-import { Link } from "expo-router";
-import { Image, ScrollView, StatusBar, Text, View } from "react-native";
+import { Image, ScrollView, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import CustomButton from "../components/CustomButton";
 
 import { images } from "../constants";
+import { router } from "expo-router";
 
 export default function Index() {
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView contentContainerStyle={{ height: "100%" }}>
         <View className="w-full justify-center items-center h-full px-4">
+          {/* Logo Image */}
           <Image
             source={images.logo}
             className="w-[130px] h-[84px]"
             resizeMode="contain"
           />
 
+          {/* Cards Image */}
           <Image
             source={images.cards}
             className="max-w-[380px] w-full h-[300px]"
             resizeMode="contain"
           />
 
+          {/* Explain Text */}
           <View className="relative mt-5">
             <Text className="text-3xl text-white font-bold text-center">
               Discover Endless Possibilities with
@@ -34,10 +38,18 @@ export default function Index() {
             />
           </View>
 
+          {/* Smaill Explain Text */}
           <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">
             Where creativity meets innovation: embark on a journey of limitless
             exploration with Aora
           </Text>
+
+          {/* Button */}
+          <CustomButton
+            title="Continue with Email"
+            handlePress={() => router.push("/sign-in")}
+            containerStyles="w-full mt-7"
+          />
         </View>
       </ScrollView>
     </SafeAreaView>
